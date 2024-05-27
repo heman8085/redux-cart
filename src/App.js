5fa61b48d68464f35ead29084d33c703f84fd171
himanshu shukla <hemi8085@gmail.com>
2024-05-27 19:29:55 +0530

    Doing Network calls and adding response the right way

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
-  const showCart = useSelector(state=> state.ui.cartIsVisible)
+  useEffect(() => {
+    fetch("https://redux-cart-a46f9-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
-     {showCart && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
